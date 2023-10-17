f8d5b06d753d13717b52300a3a935c9f1e86b62e
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-10-17 07:10:31 +0100

    Till

--- 19.2 jQuery/script.js
+++ 19.2 jQuery/script.js
@@ -1,15 +1,30 @@
-$(document).ready(() => {
-  $("h1").css("color", "red");
+// $(document).ready(() => {
+//   $("h1").css("color", "red");
+// });
+
+// $(document).ready(function () {
+//   $("h1").css("color", "green");
+// });
+
+// $(function () {
+//   $("h1").css("color", "teal");
+// });
+
+// $(() => {
+//   $("h1").css("font-size", "60px").css("padding", "0px").css("margin", "0px");
+// });
+
+$("h1").click(() => {
+  $("h1").css("background", "gray");
 });
 
-$(document).ready(function() {
-  $("h1").css("color", "green");
+$("button").click(() => {
+  $("h1").text("Loveable");
+  setTimeout(() => {
+    $("h1").html("<h1><em>I'm Serious o</em></h1>");
+  }, 1000);
 });
 
-$(function () {
-    $("h1").css("color", "teal")
-})
-
-$(() => {
-    $('h1').css('font-size', '60px').css("padding", '0px').css('margin', '0px');
-})
+$("input").keypress((event) => {
+  $("h1").text(event.key)
+});
